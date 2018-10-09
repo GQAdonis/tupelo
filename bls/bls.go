cb7de6b0dbd0dfe0fd6d3bb573b3329a8d2babfe
Brandon Westcott <brandon@quorumcontrol.com>
2018-10-09 12:50:39 -0400

    WIP

--- bls/bls.go
+++ bls/bls.go
@@ -1,8 +1,8 @@
 package bls
 
 /*
-#cgo darwin LDFLAGS: -L${SRCDIR}/../lib/darwin -lindy_crypto
-#cgo linux LDFLAGS: -L${SRCDIR}/../lib/linux -lindy_crypto
+#cgo darwin LDFLAGS: -L${SRCDIR}/lib/darwin -lindy_crypto
+#cgo linux LDFLAGS: -L${SRCDIR}/lib/linux -lindy_crypto
 #include "./bls.h"
 */
 import "C"
